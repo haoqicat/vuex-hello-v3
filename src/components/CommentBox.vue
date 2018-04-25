a02e8d3761ea9f612796e9a58b47280697a5188d
Peter Wang <happypeter1983@gmail.com>
2018-04-25 16:02:26 +0800

    add input

--- src/components/CommentBox.vue
+++ src/components/CommentBox.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="wrap">
+    <div class="comment-form">
+      <input placeholder="请填写评论" />
+      <button @click="handleClick">提交</button>
+    </div>
+   
     <div :key="comment.id" v-for="comment in reversedComments" >
       {{ comment.body }}
     </div>
@@ -25,6 +30,11 @@ export default {
     reversedComments: function() {
       return this.comments.reverse()
     }
+  },
+  methods: {
+    handleClick: () => {
+      console.log('clicked')
+    }
   }
 }
 </script>
@@ -37,5 +47,33 @@ export default {
   width: 400px;
   margin: 20px auto;
   padding: 10px;
+}
+
+.comment-form {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.comment-form input {
+  flex-grow: 1;
+  border: 0;
+  border-bottom: 1px solid lavender;
+}
+
+input:focus {
+  border: 0;
+  border-bottom: 1px solid #d47490;
+  outline: 0;
+}
+
+.comment-form button {
+  margin-left: 5px;
+  color: white;
+  background-color: #d47490;
+  padding: 5px 10px;
+}
+
+button:focus {
+  outline: 0;
 }
 </style>
