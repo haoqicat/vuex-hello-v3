7dc56a864679acc464afdd2f8dac97d2186cc779
Billie Zhang <billiecoder@gmail.com>
2018-04-27 15:38:55 +0800

    loadPosts

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    HOME
-    <router-link to="/post/1">文章1</router-link>
-    <router-link to="/post/2">文章2</router-link>
+    {{ posts }}
   </div>
 </template>
 
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  computed: {
+    posts() {
+      return this.$store.state.post.all
+    }
+  }
 }
 </script>
--- src/store/modules/post.js
+++ src/store/modules/post.js
@@ -1,14 +1,30 @@
+import axios from 'axios'
+
 const state = {
   all: []
 }
 
 const actions = {
-  loadPosts() {
-    return null
+  loadPosts({ commit }) {
+    axios.get('http://localhost:3008/posts').then(
+      res => {
+        console.log('res....', res.data)
+        const posts = res.data
+        commit('loadPosts', posts)
+      }
+    )
+  }
+}
+
+const mutations = {
+  loadPosts(state, posts) {
+    console.log('mutations', posts)
+    state.all = posts
   }
 }
 
 export default {
   state,
-  actions
+  actions,
+  mutations
 }
